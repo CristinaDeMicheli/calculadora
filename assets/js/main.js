450ed24433565e53b31be1f58fe57e5ff35ace1b
Cristina De Micheli <43018839+CristinaDeMicheli@users.noreply.github.com>
2022-10-05 19:20:00 -0300

    mostrar numeros en el imput

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+class CalculadoraBasica {
+
+    constructor() {
+        this.basicOperationShape = new RegExp("(([1-9][0-9]*|[0.])(.[0-9]*[1-9])?[\-\+\*\/])(([1-9][0-9]*|[0.])(.[0-9]*[1-9])?)");
+        this.memoryRegister = 0;
+    }
+
+    printMemoryContents() {
+        this.clearDisplay();
+        this.writeToDisplay(this.memoryRegister);
+    }
+
+    subtractFromMemory() {
+        this.memoryRegister -= this.solveOperation();
+    }
+
+    addToMemory() {
+        this.memoryRegister += this.solveOperation();
+    }
+
+    writeToDisplay(data) {
+        let legacy = document.getElementById("displayBox").value;
+        if (data == ".") {
+            legacy += data;
+        } else {
+            legacy = legacy == "0" ? data : legacy += data;
+        }
+        document.getElementById("displayBox").value = legacy;
+    }
+      clearDisplay() {
+        document.getElementById("displayBox").value = "0";
+    }
+
+    }
+
+    const calculadora = new CalculadoraBasica ();
